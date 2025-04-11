bf3d3cf74b12c3d0b72093a74546051f8790475c
NgocBui2701 <ngocbui27012109@gmail.com>
2025-04-11 08:51:28 +0700

    1.1

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php 
+    session_start();
+    // if (!isset($_SESSION['user'])) {
+    //     header('Location: login.php');
+    //     exit;
+    // }
     include 'view/header.php';
-    include 'view/home.php';
+    $page = isset($_GET['page']) ? $_GET['page'] : 'home';
+    switch ($page) {
+        case 'home':
+            include 'view/home.php';
+            break;
+        case 'about':
+            include 'view/about.php';
+            break;
+        case 'service':
+            include 'view/service.php';
+            break;
+        case 'menu':
+            include 'view/menu.php';
+            break;
+        case 'booking':
+            include 'view/booking.php';
+            break; 
+        case 'contact':
+            include 'view/contact.php';
+            break;
+        default:
+            echo "<div class='container mt-5'><h4>404 - Trang không tồn tại</h4></div>";
+            break;
+    }
     include 'view/footer.php';
 ?>
--- view/header.php
+++ view/header.php
@@ -6,28 +6,28 @@
   <title>Trang chủ | Vinica Restaurant</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css">
-  <link rel="icon" type="image/png" href="../img/logo.png">
-  <link rel="stylesheet" type="text/css" href="../css/styles.css">
+  <link rel="icon" type="image/png" href="layout/img/logo.png">
+  <link rel="stylesheet" type="text/css" href="layout/css/styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
   <div class="container-fluid">
     <nav class="navbar navbar-expand-lg bg-transparent sticky-top mt-3 w-100">
       <div class="container-fluid">
-        <a class="navbar-brand"  data-aos="slide-right" id="name" href="#">Vinica</a>
+        <a class="navbar-brand"  data-aos="slide-right" id="name" href="index.php?page=home">Vinica</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarScroll">
           <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;" data-aos="fade-up">
             <li class="nav-item items">
-              <a class="nav-link active" aria-current="page" href="#">Trang chủ</a>
+              <a class="nav-link active" aria-current="page" href="index.php?page=home">Trang chủ</a>
             </li>
             <li class="nav-item items" data-aos="fade-up">
-              <a class="nav-link" href="#">Không gian</a>
+              <a class="nav-link" href="index.php?page=about">Không gian</a>
             </li>
             <li class="nav-item dropdown" data-aos="fade-up">
-              <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a class="nav-link" href="index.php?page=service" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Dịch vụ
               </a>
               <ul class="dropdown-menu">
@@ -37,7 +37,7 @@
               </ul>
             </li>
             <li class="nav-item dropdown" data-aos="fade-up">
-              <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a class="nav-link" href="index.php?page=menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Thực đơn
               </a>
               <ul class="dropdown-menu">
@@ -50,55 +50,10 @@
             </li>
           </ul>
           <div class="d-flex" data-aos="slide-left">
-              <a href="#" class="btn btn-dark">Đặt bàn</a>
-              <a href="#" class="btn active ms-1 me-5" role="button" data-bs-toggle="button" aria-pressed="true">Liên hệ</a>
+              <a href="index.php?page=booking" class="btn btn-dark">Đặt bàn</a>
+              <a href="index.php?page=contact" class="btn active ms-1 me-5" role="button" data-bs-toggle="button" aria-pressed="true">Liên hệ</a>
           </div>
         </div>
       </div>
     </nav>
   </div>
-  <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg bg-transparent sticky-top mt-3 w-100">
-      <div class="container-fluid">
-        <a class="navbar-brand"  data-aos="slide-right" id="name" href="#">Vinica</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarScroll">
-          <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;" data-aos="fade-up">
-            <li class="nav-item items">
-              <a class="nav-link active" aria-current="page" href="#">Trang chủ</a>
-            </li>
-            <li class="nav-item items" data-aos="fade-up">
-              <a class="nav-link" href="#">Không gian</a>
-            </li>
-            <li class="nav-item dropdown" data-aos="fade-up">
-              <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dịch vụ
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Tổ Chức Tiệc Sinh Nhật</a></li>
-                <li><a class="dropdown-item" href="#">Tổ Chức Sự Kiện Doanh Nghiệp</a></li>
-                <li><a class="dropdown-item" href="#">Tổ Chức Tiệc Tất Niên</a></li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown" data-aos="fade-up">
-              <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Thực đơn
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Set Lunch</a></li>
-                <li><a class="dropdown-item" href="#">Set Dinner</a></li>
-                <li><a class="dropdown-item" href="#">Set Menu Gọi Món</a></li>
-                <li><a class="dropdown-item" href="#">Set Menu Tiệc</a></li>
-                <li><a class="dropdown-item" href="#">Set Buffet</a></li>
-              </ul>
-            </li>
-          </ul>
-          <div class="d-flex" data-aos="slide-left">
-              <a href="#" class="btn btn-dark">Đặt bàn</a>
-              <a href="#" class="btn active ms-1 me-5" role="button" data-bs-toggle="button" aria-pressed="true">Liên hệ</a>
-          </div>
-        </div>
-      </div>
-    </nav>
